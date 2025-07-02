c0ac4290749c06c996491719626d051ab6244df0
Elizabeth Dzhuravel <eliz5678@gmail.com>
2025-07-02 16:00:02 +0300

    initial commit

--- src/validation/createContact.js
+++ src/validation/createContact.js
@@ -2,13 +2,17 @@ import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.string.min(1).max(12).required(),
+  phoneNumber: Joi.string().min(1).max(12).required(),
   email: Joi.string().required(),
   isFavourite: Joi.string().min(3).max(20).required(),
-  contactType: {
-    type: String,
-    enum: ['work', 'home', 'personal'],
-    required: true,
-    default: 'personal',
-  },
+  //   contactType: {
+  //     type: String,
+  //     enum: ['work', 'home', 'personal'],
+  //     required: true,
+  //     default: 'personal',
+  //   },
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .default('personal')
+    .required(),
 });
