bf8b97148faf9c63ec72fc832ed1f8363729bbe4
Elizabeth Dzhuravel <eliz5678@gmail.com>
2025-07-06 00:44:57 +0300

    initial commit

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,5 +1,5 @@
 import createHttpError from 'http-errors';
-import { Session } from '../db/models/session.js';
+import { Session } from '../models/session.js';
 import { User } from '../db/models/user.js';
 
 export const authenticate = async (req, res, next) => {
