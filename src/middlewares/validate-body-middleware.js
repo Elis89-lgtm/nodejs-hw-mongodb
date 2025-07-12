75508244aacb1db6980e50293ff7b6e35b720706
Elizabeth Dzhuravel <eliz5678@gmail.com>
2025-07-12 17:10:17 +0300

    Done

--- src/middlewares/validate-body-middleware.js
+++ src/middlewares/validate-body-middleware.js
@@ -1,5 +1,7 @@
 export const validateBody = (schema) => async (req, res, next) => {
   try {
+    console.log(req.body);
+
     await schema.validateAsync(req.body, {
       abortEarly: false,
       convert: false,
--- src/utils/save-file.js
+++ src/utils/save-file.js
@@ -1,5 +1,7 @@
 import { saveFileToCloudinary } from './save-file-to-cloudinary.js';
 
 export const saveFile = async (file) => {
+  console.log(file);
+
   return await saveFileToCloudinary(file);
 };
